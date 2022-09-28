e71b5ad008fac821943e875ba7f34564d0275d41
hshine1226 <hshine1226@gmail.com>
2022-09-28 22:55:19 +0900

    오리 슈퍼클래스 추가 - 꽥꽥 소리와 수영하는 메소드는 모든 오리가 동일하다고 생각 - 생김새는 다르니까 추상 메소드로 정의

---
+++ StrategyPatternStudy/Duck.ts
@@ -0,0 +1,18 @@
+// Duck 이라는 슈퍼클래스 작성
+export default abstract class Duck {
+    constructor() {}
+
+    // 모든 오리는 꽥꽥 소리를 낼 수 있음
+    public quack() {
+        return '오리가 꽥꽥거립니다.'
+    }
+
+    // 모든 오리는 헤엄칠 수 있음
+    public swim() {
+        return '오리가 헤엄칩니다.'
+    }
+
+    // 모든 오리는 다르게 생겼으므로 보여주는 메소드는 추상 메소드로 정의
+    // 각각 오리 서브 클래스에서 구체적인 모양을 정의
+    public abstract display(): string
+}
