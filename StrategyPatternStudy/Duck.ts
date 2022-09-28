dbff0f95aec72572d2cc0e1cdca672b06e0ce841
hshine1226 <hshine1226@gmail.com>
2022-09-28 23:04:41 +0900

    모든 오리가 날 수 있도록 Duck 슈퍼 클래스에 fly 메소드 작성

--- StrategyPatternStudy/Duck.ts
+++ StrategyPatternStudy/Duck.ts
@@ -12,6 +12,11 @@ export default abstract class Duck {
         return '오리가 헤엄칩니다.'
     }
 
+    // 모든 오리가 날 수 있도록 슈퍼 클래스에 fly 메소드 작성!
+    public fly() {
+        return '오리가 날아갑니다.'
+    }
+
     // 모든 오리는 다르게 생겼으므로 보여주는 메소드는 추상 메소드로 정의
     // 각각 오리 서브 클래스에서 구체적인 모양을 정의
     public abstract display(): string
