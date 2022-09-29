1c3c9157ef4c3b7c4bdc44826cc3263efe6dfc93
hshine1226 <hshine1226@gmail.com>
2022-09-29 09:59:42 +0900

    flyBehavior, quackBehavior를 Duck 클래스에 위임(Delegate)한다.

--- StrategyPatternStudy/Duck.ts
+++ StrategyPatternStudy/Duck.ts
@@ -1,6 +1,24 @@
+import FlyBehavior from './FlyBehavior.ts/FlyBehavior'
+import QuackBehavior from './QuackBehavior/QuackBehavior'
+
 // Duck 이라는 슈퍼클래스 작성
 export default abstract class Duck {
+    // 나는 행동, 꽥꽥거리는 행동을 정의한 인터페이스를 레퍼런스 변수로 선언한다.
+    // 같은 패키지에 속하는 모든 서브클래스에서 이 레퍼런스 변수를 상속 받을 수 있음
+    public flyBehavior: FlyBehavior
+    public quackBehavior: QuackBehavior
+
     constructor() {}
+
+    // 오리 클래스에 perform 메소드를 만들어서 참조되는 객체의 행동을 위임한다.
+    // 꽥꽥거리는 행동을 하고 싶을 때는 슈퍼 클래스에서 객체의 종류에는 전혀 신경쓰지 않고 참조되는 객체에서 꽥꽥거리도록 하면 된다.
+    public performQuack(): string {
+        return this.flyBehavior.fly()
+    }
+
+    public performFly(): string {
+        return this.flyBehavior.fly()
+    }
 
     // 모든 오리는 꽥꽥 소리를 낼 수 있음
     public quack() {
