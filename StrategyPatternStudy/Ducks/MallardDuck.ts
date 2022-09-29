3e525970b85f3540ad79745e99a2d61d00cda22f
hshine1226 <hshine1226@gmail.com>
2022-09-29 10:07:28 +0900

    청동오리 클래스 생성자에서 레퍼런스 변수에 실제 행동 인스턴스를 할당

--- StrategyPatternStudy/Ducks/MallardDuck.ts
+++ StrategyPatternStudy/Ducks/MallardDuck.ts
@@ -1,8 +1,15 @@
 import Duck from '../Duck'
+import FlyWithWings from '../FlyBehavior.ts/FlyWithWings'
+import Quack from '../QuackBehavior/Quack'
 
 export default class MallardDuck extends Duck {
+    // 청동오리 생성자를 통해서 quackBehavior에 Quack 클래스를 사용하게 하고
+    // flyBehavior에 FlyWithWings를 사용하게 함으로써
+    // 실제 Duck 클래스에서 performQuack() 메소드가 호출 될 때 Quack(), FlyWithWings()가 호출되게 된다.
     constructor() {
         super()
+        this.quackBehavior = new Quack()
+        this.flyBehavior = new FlyWithWings()
     }
 
     public display(): string {
