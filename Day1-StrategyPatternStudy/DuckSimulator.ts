0f0b685333ce9a41aa24793f600adb00b7665ddd
hshine1226 <hshine1226@gmail.com>
2022-09-29 11:37:05 +0900

    duckSimulator에 display method 호출하라

--- Day1-StrategyPatternStudy/DuckSimulator.ts
+++ Day1-StrategyPatternStudy/DuckSimulator.ts
@@ -2,6 +2,7 @@ import MallardDuck from './Ducks/MallardDuck'
 import FlyRocketPowered from './FlyBehaviors/FlyRocketPowered'
 
 const mallard = new MallardDuck()
+console.log(mallard.display())
 console.log(mallard.performFly())
 mallard.setFlyBehavior(new FlyRocketPowered())
 console.log(mallard.performFly())
