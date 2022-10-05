51d67f3365cfb43b3d221df973cfd48ffc11b095
hshine1226 <hshine1226@gmail.com>
2022-10-05 20:41:30 +0900

    CondimentDecorator 클래스명 변경

--- Day2-DecoratorPatternStudy/CondimentDecorator.ts
+++ Day2-DecoratorPatternStudy/CondimentDecorator.ts
@@ -1,6 +1,6 @@
 import Beverage from './Beverage'
 
-export default abstract class CondimentDecorators extends Beverage {
+export default abstract class CondimentDecorator extends Beverage {
     public beverage: Beverage
     public abstract getDescription(): string
 }
