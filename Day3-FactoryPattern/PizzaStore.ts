fc90ff190fb0810e03c67aa595ddbc0cbf0cee7e
hshine1226 <hshine1226@gmail.com>
2022-10-05 22:04:37 +0900

    피자 가게의 피자 만드는 부분을 팩토리로 대체

--- Day3-FactoryPattern/PizzaStore.ts
+++ Day3-FactoryPattern/PizzaStore.ts
@@ -1,8 +1,17 @@
 import Pizza from './Pizza'
+import { SimplePizzaFactory } from './SimplePizzaFactory'
 
 export default class PizzaStore {
+    private factory: SimplePizzaFactory
+
+    constructor(factory: SimplePizzaFactory) {
+        this.factory = factory
+    }
+
     public orderPizza(type: string) {
         let pizza: Pizza
+
+        pizza = this.factory.createPizza(type)
 
         pizza.prepare()
         pizza.bake()
