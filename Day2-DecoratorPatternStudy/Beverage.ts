c2b7a53775caf931e22b7712cfc2790cbb19bc92
hshine1226 <hshine1226@gmail.com>
2022-10-05 20:56:15 +0900

    Beverage 슈퍼 클레스에 사이즈 getter, setter 추가

--- Day2-DecoratorPatternStudy/Beverage.ts
+++ Day2-DecoratorPatternStudy/Beverage.ts
@@ -1,11 +1,27 @@
+enum Size {
+    TALL,
+    GRANDE,
+    VENTI
+}
 export default abstract class Beverage {
     public description: string
+    public size: Size
 
-    constructor() {}
+    constructor() {
+        this.size = Size.TALL
+    }
 
     public getDescription(): string {
         return this.description
     }
 
     public abstract cost(): number
+
+    public setSize(size: Size) {
+        this.size = size
+    }
+
+    public getSize(): Size {
+        return this.size
+    }
 }
