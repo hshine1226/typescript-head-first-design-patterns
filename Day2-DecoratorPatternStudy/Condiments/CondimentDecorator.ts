1401538252dd6e12fc649821950d33f3dcc6e2d1
hshine1226 <hshine1226@gmail.com>
2022-10-05 21:13:32 +0900

    CondimentDecorator에 getSize() 메소드 추가

--- Day2-DecoratorPatternStudy/Condiments/CondimentDecorator.ts
+++ Day2-DecoratorPatternStudy/Condiments/CondimentDecorator.ts
@@ -1,6 +1,15 @@
-import Beverage from '../Beverage'
+import Beverage, { Size } from '../Beverage'
 
 export default abstract class CondimentDecorator extends Beverage {
+    constructor() {
+        super()
+    }
+
     public beverage: Beverage
+
     public abstract getDescription(): string
+
+    public getSize(): Size {
+        return this.beverage.getSize()
+    }
 }
