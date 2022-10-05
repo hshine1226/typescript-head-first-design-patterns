f6e368de80f97b5ea2fac8d2731ab686f9c1a9c1
hshine1226 <hshine1226@gmail.com>
2022-10-05 19:06:31 +0900

    Beverage 슈퍼 클래스 재정의 하라 for 첨가물 데코레이터

--- Day2-DecoratorPatternStudy/Beverage.ts
+++ Day2-DecoratorPatternStudy/Beverage.ts
@@ -1,73 +1,11 @@
 export default abstract class Beverage {
     public description: string
-    private milk: boolean
-    private milkPrice: number
-    private soy: boolean
-    private soyPrice: number
-    private mocha: boolean
-    private mochaPrice: number
-    private whip: boolean
-    private condimentPrice: number
 
-    constructor() {
-        this.milk = false
-        this.milkPrice = 300
-        this.soy = false
-        this.soyPrice = 300
-        this.mocha = false
-        this.mochaPrice = 200
-        this.whip = false
-        this.condimentPrice = 0
-    }
+    constructor() {}
 
     public getDescription(): string {
         return this.description
     }
 
-    private hasMilk(): boolean {
-        if (this.milk) {
-            return true
-        }
-        return false
-    }
-
-    public setMilk() {
-        this.milk = true
-    }
-
-    private hasSoy(): boolean {
-        if (this.soy) {
-            return true
-        }
-        return false
-    }
-
-    public setSoy() {
-        this.soy = true
-    }
-
-    private hasMocha(): boolean {
-        if (this.mocha) {
-            return true
-        }
-        return false
-    }
-
-    public setMocha() {
-        this.mocha = true
-    }
-
-    public cost(): number {
-        if (this.hasMilk()) {
-            this.condimentPrice += this.milkPrice
-        }
-        if (this.hasSoy()) {
-            this.condimentPrice += this.soyPrice
-        }
-        if (this.hasMocha()) {
-            this.condimentPrice += this.mochaPrice
-        }
-
-        return this.condimentPrice
-    }
+    public abstract cost(): number
 }
