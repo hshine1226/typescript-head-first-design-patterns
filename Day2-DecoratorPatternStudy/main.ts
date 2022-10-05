4728b47ad7a452457ec312944ee31df44b55a4a8
hshine1226 <hshine1226@gmail.com>
2022-10-05 20:48:10 +0900

    데코레이터를 사용하여 커피 주문 추가 (main.ts)

--- Day2-DecoratorPatternStudy/main.ts
+++ Day2-DecoratorPatternStudy/main.ts
@@ -1,16 +1,16 @@
+import Mocha from './Condiments/Mocha'
+import Whip from './Condiments/Whip'
 import DarkRoast from './DarkRoast'
-import Decaf from './Decaf'
+import Espresso from './Espresso'
+
+const espresso = new Espresso()
+console.log(espresso.getDescription(), espresso.cost(), '원')
 
 const darkRoast = new DarkRoast()
-console.log(darkRoast.getDescription())
-console.log(darkRoast.cost())
-darkRoast.setMilk()
-console.log(darkRoast.cost())
-darkRoast.setMocha()
-console.log(darkRoast.cost())
-
-const decaf = new Decaf()
-console.log(decaf.getDescription())
-console.log(decaf.cost())
-decaf.setMilk()
-console.log(decaf.cost())
+console.log(darkRoast.getDescription(), darkRoast.cost(), '원')
+const beverage1 = new Mocha(darkRoast)
+console.log(beverage1.getDescription(), beverage1.cost(), '원')
+const beverage2 = new Mocha(beverage1)
+console.log(beverage2.getDescription(), beverage2.cost(), '원')
+const beverage3 = new Whip(beverage2)
+console.log(beverage3.getDescription(), beverage3.cost(), '원')
