8de8133f7e6952e771a5c68956985176ff530681
hshine1226 <hshine1226@gmail.com>
2022-10-12 23:19:32 +0900

    Singleton uniqueInstance와 getInstance를 static으로 변경

--- Day3-SingletonPatternStudy/Singleton.ts
+++ Day3-SingletonPatternStudy/Singleton.ts
@@ -1,13 +1,13 @@
 export class Singleton {
     // Singleton 클래스의 하나뿐인 인스턴스를 저장하는 정적 변수
     // uniqueInstance가 정적 변수라는 것을 잊지 말아야 함
-    private uniqueInstace: Singleton
+    private static uniqueInstace: Singleton
 
     // 생성자를 private로 선언했으므로 Singleton에서만 클래스의 인스턴스를 만들 수 있다.
     private constructor() {}
 
     // getInstance() 메소드는 클래스의 인스턴스를 만들어서 return 한다.
-    public getInstance(): Singleton {
+    public static getInstance(): Singleton {
         // lazyinstantiation (게으른 인스턴스 생성)
         // 인스턴스가 필요한 상황이 닥치기 전까지 인스턴스를 생성하지 않음
         if (this.uniqueInstace == null) this.uniqueInstace = new Singleton()
